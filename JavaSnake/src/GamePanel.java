9a5eb9d9439ec0600e1d0b21cd66fdc5d07501c1
Hector H <102716847+hectorwithc@users.noreply.github.com>
2022-05-27 13:35:02 +0200

    Add files via upload

---
+++ JavaSnake/src/GamePanel.java
@@ -0,0 +1,260 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	static final int SCREEN_WIDTH = 800;
+	static final int SCREEN_HEIGHT = 800;
+	static final int UNIT_SIZE = 40;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
+	// Easter eggs
+	boolean randomColorsMode = false;
+	
+	// Settings
+	boolean gridMode = false;
+	boolean noBordersMode = true;
+	
+	// Restart
+	
+	GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
+	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
+	}
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+	public void draw(Graphics g) {
+		
+		if(running) {
+			
+			if (gridMode) {
+				// Draw a grid
+				for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+					g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+					g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+				}
+			}
+			
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+			
+			for(int i = 0; i< bodyParts; i++) {
+				if (i == 0) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				} else {
+					g.setColor(new Color(45, 180, 0));
+					if(randomColorsMode) {
+						g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+					}
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}	
+			
+			// Game score text
+			g.setColor(Color.white);
+			g.setFont(new Font("Poppins", Font.BOLD, 30));
+			FontMetrics metrics1 = getFontMetrics(g.getFont());
+			g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
+			
+			// Settings and Easter eggs activated text
+			g.setColor(new Color(45, 180, 0));
+			g.setFont(new Font("Poppins", Font.BOLD, 15));
+			if (noBordersMode) {
+				g.drawString("No Borders", (SCREEN_WIDTH/UNIT_SIZE)-1, (SCREEN_HEIGHT - 10));
+			}
+		}
+		else {
+			gameOver(g);
+		}
+		
+	}
+	public void newApple() {
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+	}
+	public void move() {
+		for(int i = bodyParts;i>0;i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		
+		switch(direction) {
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
+		
+	}
+	public void checkApple() {
+		if((x[0] == appleX) && (y[0] == appleY)) {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
+	}
+	public void checkCollisions() {
+		// checks if head collides with body
+		for(int i = bodyParts; i>0; i--) {
+			if ((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		
+		if (noBordersMode) {
+			// check if head touches left border
+			if (x[0] < 0) {
+				x[0] = x[0] + (SCREEN_WIDTH + (1 * UNIT_SIZE));
+				direction = 'L';
+			}
+			// check if head touches right border
+			if (x[0] > SCREEN_WIDTH) {
+				x[0] = x[0] - (SCREEN_WIDTH + (1 * UNIT_SIZE));
+				direction = 'R';
+			}
+			// check if head touches top border
+			if (y[0] < 0) {
+				y[0] = y[0] + (SCREEN_HEIGHT + (1 * UNIT_SIZE));
+				direction = 'U';
+			}
+			// check if head touches bottom border
+			if (y[0] > SCREEN_HEIGHT) {
+				y[0] = y[0] - (SCREEN_HEIGHT + (1 * UNIT_SIZE));
+				direction = 'D';
+			}
+		} else {
+			// check if head touches left border
+			if (x[0] < 0) {
+				running = false;
+			}
+			// check if head touches right border
+			if (x[0] > SCREEN_WIDTH) {
+				running = false;
+			}
+			// check if head touches top border
+			if (y[0] < 0) {
+				running = false;
+			}
+			// check if head touches bottom border
+			if (y[0] > SCREEN_HEIGHT) {
+				running = false;
+			}
+		}
+		
+		if (!running) {
+			timer.stop();
+		}
+	}
+	public void gameOver(Graphics g) {
+		// Game over text
+		g.setColor(new Color(255, 32, 32));
+		g.setFont(new Font("Poppins", Font.BOLD, 75));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+		// Game score text
+		g.setColor(Color.white);
+		g.setFont(new Font("Poppins", Font.BOLD, 40));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			// Easter eggs
+			case KeyEvent.VK_C:
+				if(randomColorsMode) {
+					randomColorsMode = false;
+				} else {
+					randomColorsMode = true;
+				}
+				break;
+			// Settings
+			case KeyEvent.VK_G:
+				if (gridMode) {
+					gridMode = false;
+				} else {
+					gridMode = true;
+				}
+				break;
+			case KeyEvent.VK_B:
+				if (noBordersMode) {
+					noBordersMode = false;
+				} else {
+					noBordersMode = true;
+				}
+				break;
+			}
+		}
+	}
+
+}
